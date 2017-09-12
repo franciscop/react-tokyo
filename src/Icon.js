ea3714886d69318000bfde0e411599ee40d4b206
Francisco Presencia <public@francisco.io>
2017-09-12 22:12:15 +0900

    Changes

--- src/Icon.js
+++ src/Icon.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 
-export default (props) => {
-  const liked = () => {
-    alert('Liked!')
-  };
-  return (
-    <img onClick={liked} src={props.src} />
-  );
-};
+import './Icon.css';
+
+export default class Icon extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      liked: false
+    };
+  }
+  render () {
+    const liked = () => {
+      this.setState({
+        liked: !this.state.liked
+      });
+    };
+
+    return (
+      <img
+        className={this.state.liked ? "icon liked" : "icon"}
+        onClick={liked}
+        src={this.props.src}
+        />
+    );
+  }
+}
+
+//
+// if (a) {
+//   alert('Hello');
+// } else {
+//   alert('World');
+// }
+//
+// a ? alert('Hello') : alert('World');
+//
+//
+// alert(a ? 'Hello' : 'World');
+
+
+
+// export default (props) => {
+//   const liked = () => {
+//     alert('Liked!')
+//   };
+//   return (
+//     <img className="icon" onClick={liked} src={props.src} />
+//   );
+// };
